78a3f0b67ec13cfe478b2dcd383580db5a9e25ec
hani <43071037+haniwa2021@users.noreply.github.com>
2021-10-14 13:18:41 +0900

    add webpack.config.js add css-loader / style-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,25 @@
 const path = require('path');
+const { kill } = require('process');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, './dist'),
-    }
+        filename: 'main.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css/,  // .cssを検知したら…
+                use: [          // -> css-loaderを使う
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                    },  // 下から適応されるのに注意！！
+                ],
+            },
+        ],
+    },
 }
