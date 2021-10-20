5032539f7098bb733328091e9dbab6e1a1dbe93e
hani <43071037+haniwa2021@users.noreply.github.com>
2021-10-20 23:42:00 +0900

    Forget webpack.config.js.

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     entry: "./src/javascripts/main.js",
     output: {
         path: path.resolve(__dirname, "./dist"),
-        filename: "./javascritpts/main.js",
+        filename: "javascritpts/[name]-[hash].js",
     },
     module: {
         rules: [
@@ -66,7 +66,7 @@ module.exports = {
                         loader: "file-loader",
                         options: {
                             esModule: false,
-                            name: "images/[name].[ext]",
+                            name: "images/[name]-[hash].[ext]",
                             publicPath: '/',
                         },
                     },
@@ -93,7 +93,7 @@ module.exports = {
     },
     plugins: [
         new MiniCssExtractPlugin({
-            filename: "./stylesheets/main.css",
+            filename: "./stylesheets/[name]-[hash].css",
         }),
         new HtmlWebpackPlugin({
             template: "./src/templates/index.pug",
